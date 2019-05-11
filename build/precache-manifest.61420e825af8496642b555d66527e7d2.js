7c2375b5676ff03ba896cac028d1a53b67c2b08a
Mike <mikeinhistory@gmail.com>
2019-05-11 17:08:20 -0500

    deployed needs image resizing

---
+++ build/precache-manifest.61420e825af8496642b555d66527e7d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a11107970073e4ca6978473edde0745f",
+    "url": "/HomeMoviesGame/index.html"
+  },
+  {
+    "revision": "dfbfa583dfda629bacfc",
+    "url": "/HomeMoviesGame/static/css/main.e514c1a4.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/HomeMoviesGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "dfbfa583dfda629bacfc",
+    "url": "/HomeMoviesGame/static/js/main.bb3f53ea.chunk.js"
+  },
+  {
+    "revision": "99f621542c5b711326cd",
+    "url": "/HomeMoviesGame/static/js/runtime~main.f7d082f6.js"
+  }
+]);
