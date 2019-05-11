d3aefc37fc9b4a65c5d1b406abc2847ff1c47d77
Mike <mikeinhistory@gmail.com>
2019-05-11 15:59:20 -0500

    working finally, images need to be resized

---
+++ build/precache-manifest.3feab76f0e76621d32910f114b096214.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6d013e6fcb483357caad66a60572c2e",
+    "url": "/ClickGame/index.html"
+  },
+  {
+    "revision": "deb6cd79f1a86b271e29",
+    "url": "/ClickGame/static/css/main.e514c1a4.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/ClickGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "deb6cd79f1a86b271e29",
+    "url": "/ClickGame/static/js/main.633d739b.chunk.js"
+  },
+  {
+    "revision": "d76327d9eefc62a77850",
+    "url": "/ClickGame/static/js/runtime~main.979c4616.js"
+  }
+]);
